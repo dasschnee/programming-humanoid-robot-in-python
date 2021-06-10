ec4bfb78a6c3113cd5b31eadbed7ed312ab86999
dasschnee <jon.schnaecker@gmail.com>
2021-06-10 16:52:51 +0200

    init agent_client and server

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,21 +7,27 @@
 '''
 
 import weakref
+from xmlrpc.client import ServerProxy
+import numpy as np
+import threading
 
 class PostHandler(object):
-    '''the post hander wraps function to be excuted in paralle
+    '''the post hander wraps function to be excuted in parallel
     '''
     def __init__(self, obj):
         self.proxy = weakref.proxy(obj)
+        self.server = ServerProxy('http://localhost:8082')
 
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
         # YOUR CODE HERE
+        keyt = threading.Thread(target=self.server.execute_keyframes, args=[keyframes.tolist()])
+        keyt.start()
 
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
-
+        self.server.set_transforms(effector_name, transform.tolist())
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
@@ -33,34 +39,42 @@ class ClientAgent(object):
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.post.server.get_angle(joint_name)
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.post.server.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.post.server.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.post.server.execute_keyframes(keyframes)
+        # times = keyframes[1]
+        # m = max([max(list) for list in times])
+        # sleep(m)
+        # self.keyframes = ([], [], [])
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.post.server.get_transform(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.post.server.set_transform(effector_name, transform)
 
 if __name__ == '__main__':
     agent = ClientAgent()
-    # TEST CODE HERE
-
-
+    agent.execute_keyframes()
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,44 +14,73 @@
 # add PYTHONPATH
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
+import numpy as np
+import time
+import threading
+from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
 
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
+    def __init__(self):
+        super(ServerAgent, self).__init__()
+        port = 8082
+        server = SimpleXMLRPCServer(
+            ('localhost', port), logRequests=True, allow_none=True)
+        server.register_instance(self)
+        server.register_introspection_functions()
+        server.register_multicall_functions()
+        print ('Starting server on port %s' % port)
+        thread = threading.Thread(target=server.serve_forever)
+        thread.start()
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.perception.joint[joint_name]
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
-        # YOUR CODE HERE
+        self.perception.joint[joint_name] = angle
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.recognize_posture(self.perception)
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyframes = keyframes
+        _ , times , _ = keyframes
+        self.angle_interpolation(keyframes, self.perception)
+        
+        longest_time  = max([max(list) for list in times])
+        time.sleep(longest_time)
+        self.keyframes = ([], [], [])
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.transforms[name]
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.set_transforms(effector_name, np.matrix(transform))
 
 if __name__ == '__main__':
     agent = ServerAgent()
